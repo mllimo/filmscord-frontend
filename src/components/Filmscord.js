b753e638413526e773287078d726eada3dc3719f
mllimo <ar.guijarro.contreras@gmail.com>
2022-01-13 17:31:55 +0000

    Configurando sass bulma

---
+++ src/components/Filmscord.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Filmscord = () => { 
+  return (
+    <>
+      <h1>Filmscord</h1>
+    </>
+  );
+};
+
+export default Filmscord;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Filmscord from './components/Filmscord';
+import './styles/css/mystyle.css';
 
 ReactDOM.render(
-  <></>,
+  <Filmscord />,
   document.getElementById('root')
 );
