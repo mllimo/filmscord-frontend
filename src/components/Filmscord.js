1e40d7a0a1b13f60262fafa7be917b9ddd34a022
mllimo <ar.guijarro.contreras@gmail.com>
2022-01-13 20:02:52 +0000

    Cmbiando scripts npm

--- src/components/Filmscord.js
+++ src/components/Filmscord.js
@@ -1,11 +1,13 @@
 import React from "react";
+import Header from "./static/Header";
 
 const Filmscord = () => { 
   return (
     <>
+      <Header/>
       <h1>Filmscord</h1>
     </>
   );
-};
+}
 
 export default Filmscord;
---
+++ src/components/static/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="columns has-background-black">
+      <div className="column has-text-centered has-text-white p-0 mt-3">
+      </div>
+
+      <div className="column has-text-centered has-text-white p-0 mt-3">
+        <h1 className="text-font-fredoka is-size-1">FILMSCORD</h1>
+      </div>
+
+
+      <div 
+        className="column is-flex is-align-items-center is-justify-content-end has-text-white p-0 mt-3 pr-5">
+          DROPDOWN COMPONENT
+      </div>
+
+    </div>
+  );
+}
+
+export default Header;
